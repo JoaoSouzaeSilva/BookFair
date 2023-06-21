19969e6198f79c0d8f62eddb4669fda55926bcc3
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2023-06-21 21:14:30 +0100

    created PostBookRequest

---
+++ src/main/kotlin/com/bookfair/controller/BookController.kt
@@ -0,0 +1,18 @@
+package com.bookfair.controller
+
+import com.bookfair.controller.request.PostBookRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("books")
+class BookController(
+
+) {
+
+    @PostMapping
+    fun create(@RequestBody request: PostBookRequest)
+
+}
