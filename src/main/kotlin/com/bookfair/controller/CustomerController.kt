9f2f50b0ea670c7966207e770f7eb30bd82a0f2d
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2023-06-21 16:08:12 +0100

    Update CustomerController.kt

--- src/main/kotlin/com/bookfair/controller/CustomerController.kt
+++ src/main/kotlin/com/bookfair/controller/CustomerController.kt
@@ -4,21 +4,28 @@ import com.bookfair.controller.request.PostCustomerRequest
 import com.bookfair.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import java.util.UUID
 
 @RestController
 @RequestMapping("customers")
 class CustomerController {
 
-    val customers = ;
+    var customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1","João","joao@gmail.com")
+    fun getCustomer(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createCustomer(@RequestBody customer: PostCustomerRequest): PostCustomerRequest {
-        return customer
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+        var newId = if (customers.isEmpty())
+                        "1"
+                    else
+                        customers.last().id + 1
+
+        customers.add(CustomerModel(newId, customer.name, customer.email))
+        println(customer)
     }
 }
