06576df4ee1873d8ef3a0982e9f700d4d5176f24
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2023-06-22 20:05:17 +0100

    Added custom validator to check if email available

--- src/main/kotlin/com/bookfair/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/bookfair/controller/request/PostCustomerRequest.kt
@@ -1,6 +1,7 @@
 package com.bookfair.controller.request
 
 import com.bookfair.model.CustomerModel
+import com.bookfair.validation.EmailAvailable
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
 
@@ -10,5 +11,6 @@ data class PostCustomerRequest(
     var name: String,
 
     @field:Email(message = "Email field must be a well formatted email address")
+    @EmailAvailable
     var email: String
 )
