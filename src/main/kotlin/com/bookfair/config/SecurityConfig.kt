3516fd1a790dd6ca4616a09cdab8b69779433dfc
João Souza e Silva <jmop.silva@campus.fct.unl.pt>
2023-06-24 20:13:00 +0100

    added security filters

--- src/main/kotlin/com/bookfair/config/SecurityConfig.kt
+++ src/main/kotlin/com/bookfair/config/SecurityConfig.kt
@@ -2,10 +2,40 @@ package com.bookfair.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+
+        http.invoke {
+
+            csrf { disable() }
+            cors { disable() }
+
+            authorizeRequests {
+                authorize(HttpMethod.POST,"/customers",permitAll)
+                authorize(anyRequest,authenticated)
+            }
+
+            sessionManagement { SessionCreationPolicy.STATELESS }
+
+        }
+
+        return http.build()
+    }
+
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
